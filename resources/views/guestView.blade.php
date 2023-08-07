6c0c102960eb7750588a09661b39058ea46ca958
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-08-07 11:44:49 -0300

    feat: adding the guest view | adding the cooper-hewitt font

---
+++ resources/views/guestView.blade.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Time Sync</title>
+    <link rel="stylesheet" href="/assets/css/styles.css">
+    <link rel="stylesheet" href="/assets/css/fonts.css">
+</head>
+
+<body>
+    <main class="container">
+        <article class="organizador__actions">
+            <header class="organizador__actions-header">
+                <div>
+                    <h1>Guilherme Wilker.</h1>
+                    <p>Agende uma call para discutirmos seu projeto!</p>
+                </div>
+
+            </header>
+
+            <div class="organizador__actions-bio">
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                    consequat.
+                </p>
+
+                <p>
+                    Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                </p>
+
+            </div>
+
+            <div class="organizador__actions-plataformas">
+                <small>Guilherme aceita as seguintes plataformas para meeting!</small>
+
+
+                <div class="plataformas">
+                    <div class="plataforma">
+                        <div class="img">
+                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Google_Meet_icon_%282020%29.svg/512px-Google_Meet_icon_%282020%29.svg.png?20221213135236"
+                                alt="plataforma de meeting, Google Meet">
+                        </div>
+
+                        <h3>Google meet</h3>
+                    </div>
+                    <div class="plataforma">
+                        <div class="img">
+                            <img src="https://www.freepnglogos.com/uploads/zoom-logo-png/zoom-meeting-logo-transparent-png-21.png"
+                                alt="plataforma de meeting, Zoom">
+                        </div>
+
+                        <h3>Zoom</h3>
+                    </div>
+                </div>
+
+            </div>
+
+            <h1 class="minutes-per-call">25min ~ 45min.</h1>
+
+        </article>
+
+        <section class="calendario">
+            <h3>Calendário</h3>
+        </section>
+    </main>
+</body>
+
+</html>
