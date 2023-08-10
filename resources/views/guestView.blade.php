6bba8b9f096cc77d99abdbf813a947faaa878454
Guilherme Wilker <wilkerguilherme0@gmail.com>
2023-08-10 15:32:56 -0300

    style: tweaks to guest view components

--- resources/views/guestView.blade.php
+++ resources/views/guestView.blade.php
@@ -12,64 +12,64 @@
 <body>
     <main class="container">
         <article class="organizador__actions">
-            <header class="organizador__actions-header">
-                <div>
-                    <h1>Guilherme Wilker.</h1>
-                    <p>Agende uma call para discutirmos seu projeto!</p>
+            <div class="organizador__actions-sidebar">
+                <header class="organizador__actions-header">
+                    <div>
+                        <h1>Guilherme Wilker.</h1>
+                        <p>Agende uma call para discutirmos seu projeto!</p>
+                    </div>
+                </header>
+
+                <div class="organizador__actions-bio">
+                    <p>
+                        Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                        quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                        consequat.
+                    </p>
+
+                    <p>
+                        Duis aute irure dolor in reprehenderit in voluptate velit esse
+                        cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                        proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+                    </p>
+
                 </div>
 
-            </header>
-
-            <div class="organizador__actions-bio">
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
-                    tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
-                    quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-                    consequat.
-                </p>
-
-                <p>
-                    Duis aute irure dolor in reprehenderit in voluptate velit esse
-                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
-                    proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                </p>
-
-            </div>
-
-            <div class="organizador__actions-plataformas">
-                <small>Guilherme aceita as seguintes plataformas para meeting!</small>
+                <div class="organizador__actions-plataformas">
+                    <small>Guilherme aceita as seguintes plataformas para meeting!</small>
 
 
-                <div class="plataformas">
-                    <div class="plataforma">
-                        <div class="img">
-                            <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Google_Meet_icon_%282020%29.svg/512px-Google_Meet_icon_%282020%29.svg.png?20221213135236"
-                                alt="plataforma de meeting, Google Meet">
+                    <div class="plataformas">
+                        <div class="plataforma">
+                            <div class="img">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/9b/Google_Meet_icon_%282020%29.svg/512px-Google_Meet_icon_%282020%29.svg.png?20221213135236"
+                                    alt="plataforma de meeting, Google Meet">
+                            </div>
+
+                            <h3>Google meet</h3>
                         </div>
+                        <div class="plataforma">
+                            <div class="img">
+                                <img src="https://www.freepnglogos.com/uploads/zoom-logo-png/zoom-meeting-logo-transparent-png-21.png"
+                                    alt="plataforma de meeting, Zoom">
+                            </div>
 
-                        <h3>Google meet</h3>
-                    </div>
-                    <div class="plataforma">
-                        <div class="img">
-                            <img src="https://www.freepnglogos.com/uploads/zoom-logo-png/zoom-meeting-logo-transparent-png-21.png"
-                                alt="plataforma de meeting, Zoom">
+                            <h3>Zoom</h3>
                         </div>
-
-                        <h3>Zoom</h3>
                     </div>
+
                 </div>
 
+                <h1 class="minutes-per-call">25min ~ 45min.</h1>
             </div>
-
-            <h1 class="minutes-per-call">25min ~ 45min.</h1>
-
         </article>
 
         <section class="calendario">
             <div>
                 <h4>
-                    Selecione algum dia disponível para notificar <br>
-                    o organizador para uma reunião!
+                    Selecione um dia disponível para notificar <br />
+                    o organizador da reunião!
                 </h4>
             </div>
             <livewire:calendar />
